f10ba1212e96dd6e3b176d786ddcf9c335240a87
Ralphd42 <rdonato@albany.edu>
2019-12-11 22:39:05 -0500

    MATDISAt

---
+++ generateSquareMatrix.c
@@ -0,0 +1,65 @@
+
+#include<stdio.h>
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+//prototype
+double getRand(double min, double max);
+
+int main(int argc, char*argv[])
+{
+  if(argc<2)
+  {
+    perror("\nUsage ./generateInput <squareMatrixSize>\n");
+    exit(-1);
+  }
+  FILE *fp;
+  int matrixSize = atoi(argv[1]), i, j;
+  double min=0.00001, max=100000;
+  char comma[2];
+  fp = fopen("inputMatrix.csv", "w");
+  srand(time(NULL));
+  fprintf(fp, "%d\r\n", matrixSize);
+  for(i=0;i<matrixSize;i++)
+  {
+    sprintf(comma, "%s", "");
+    for(j=0;j<matrixSize;j++)
+    {
+      fprintf(fp, "%s%f",comma,getRand(min, max));
+      sprintf(comma, "%s", ",");;
+    }
+      fprintf(fp, "\r\n");
+  }
+fprintf(fp, "\r\n");
+  for(i=0;i<matrixSize;i++)
+  {
+    sprintf(comma, "%s", "");
+    for(j=0;j<matrixSize;j++)
+    {
+      fprintf(fp, "%s%f",comma,getRand(min, max));
+      sprintf(comma, "%s", ",");;
+    }
+      fprintf(fp, "\r\n");
+  }
+fprintf(fp, "\r\n");
+  for(i=0;i<matrixSize;i++)
+  {
+    sprintf(comma, "%s", "");
+    for(j=0;j<matrixSize;j++)
+    {
+      fprintf(fp, "%s%f",comma,getRand(min, max));
+      sprintf(comma, "%s", ",");;
+    }
+      fprintf(fp, "\r\n");
+  }
+
+  fclose(fp);
+
+}
+
+
+double getRand(double min, double max)
+{
+    double d = (double)rand() / RAND_MAX;
+    return min + d * (max - min);
+}
---
+++ parallel/matmul.c
@@ -0,0 +1,129 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<omp.h>
+#include<string.h>
+#include "timing.h"
+
+void DisplayArray( FILE * outLocation, double * arr, int rowcolLen)
+{
+    int colcnter =0;
+    int rowcnter =0;
+    for(rowcnter=0;rowcnter<rowcolLen;rowcnter++  ){
+        for(  colcnter=0;colcnter<rowcolLen;colcnter++){        
+            if(colcnter>0){
+                fprintf(outLocation,",");    
+
+            }            
+            fprintf(outLocation, "%f" ,arr[ rowcnter*rowcolLen +colcnter]);  
+        }
+        fprintf(outLocation,"\r\n");
+    }
+}
+
+void doMatMul( double * arrA, double * arrB, double * arrC ,int rowcolLen)   
+{
+    timing_start();
+     #pragma omp parallel for
+    for (int i = 0;i<rowcolLen;++i)
+    {
+        for(int  j = 0;j<rowcolLen;++j)
+        {
+            for(int k=0;k<rowcolLen;++k)
+            {
+                *(arrC +i*rowcolLen + j)  = 
+                    *(arrC + i * rowcolLen + j)  +   
+                    *(arrA + i * rowcolLen + k) *  
+                    *(arrB + k * rowcolLen + j);
+            }
+        }
+    }
+    timing_stop();
+    print_timing();
+}
+
+/*Fill Arrays    */
+void FIllArray( FILE * inFile, double * arr, int rowcolLen)
+{
+     
+    int colcnter =0;
+     
+    int rowcnter =0;
+     
+    char line[4098];
+     
+    while (fgets(line, 4098, inFile) && (rowcnter < rowcolLen))
+    {
+        if(strlen(line)>3){
+        char * tok;
+        for (tok =strtok(line,","); colcnter<rowcolLen;colcnter++)
+        {
+            arr[ rowcnter* rowcolLen +colcnter     ] = atof(tok);    
+
+            //printf("ColNum|%d| rowNum |%d| val|%f|tok|%s|\r\n\r\n",colcnter,rowcnter, atof(tok), tok        );
+            tok =strtok(NULL,",");
+
+        } 
+        colcnter =0;
+        rowcnter++;
+        }
+    }
+}
+
+void InitArray(double * arr, int rowcolLen)
+{
+    for( int rowcnter =0; rowcnter<rowcolLen;rowcnter++)
+    {
+         
+        for( int colcnter =0; colcnter<rowcolLen;colcnter++)
+        {
+             
+            arr[ rowcnter*rowcolLen +colcnter    ] = 0;
+             
+        }    
+    }
+}
+
+
+
+int main(int argc, char*argv[])
+{
+    
+   // omp_set_num_threads(8);
+    int arrdim =0;
+    if(argc<2)
+    {
+        perror("\nUsage ./matmul <inputfile.csv>\n");
+        exit(-1);
+    }
+    char * filename= argv[1];
+     
+    // get dimentions of array
+    FILE *q;
+    q = fopen(filename, "r");
+
+    fscanf(q,"%d", &arrdim);
+    printf("%d\r\n",arrdim);
+    int numElements = arrdim*arrdim;
+    printf("%d\r\n",numElements);
+    // matrixes
+    double * matA   =((double *) malloc(sizeof(double)*numElements));
+    double * matB   =((double *) malloc(sizeof(double)*numElements));
+    double * matC   =((double *) malloc(sizeof(double)*numElements));
+    FIllArray(q,matA,arrdim);
+    FIllArray(q,matB,arrdim);
+    InitArray(matC,arrdim);
+    doMatMul( matA, matB, matC ,arrdim);
+    FILE * Output;
+    char * outfileName ="outFile.txt";
+    Output = fopen(outfileName, "w");
+    DisplayArray(Output,matC,arrdim);
+    fclose(Output);
+    free(matA  );
+    free(matB  );
+    free(matC  );
+    fclose(q);
+    return 1;
+}
+
+
+https://edoras.sdsu.edu/~mthomas/sp17.605/lectures/MPI-MatMatMult.pdf
